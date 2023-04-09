00a5a62ea87097674aa640fec5a5ece361626483
THowland1 <tomhowland9@gmail.com>
2023-04-09 09:13:40 +0100

    💅 Add tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script>
+	import '../app.css';
+</script>
+
+<div class="absolute inset-0 bg-neutral-800 text-neutral-200 grid">
+	<div class="col-start-1 row-start-1 overflow-auto">
+		<slot />
+	</div>
+</div>
